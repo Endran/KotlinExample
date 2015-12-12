1ce0a8bffc2d843c8be475af15bdb2b2c31f3d35
David Hardy <david.hardy@philips.com>
2015-12-12 21:19:03 +0100

    Step 3; Move fab code into example class using constructor and execute via method

--- Sources/KotlinExample/app/src/main/java/nl/endran/kotlinexample/Example.kt
+++ Sources/KotlinExample/app/src/main/java/nl/endran/kotlinexample/Example.kt
@@ -1,2 +1,11 @@
 package nl.endran.kotlinexample
 
+import android.support.design.widget.FloatingActionButton
+import android.support.design.widget.Snackbar
+
+class Example(val fab: FloatingActionButton) {
+
+    fun doMagic() {
+        fab.setOnClickListener { view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show() }
+    }
+}
--- Sources/KotlinExample/app/src/main/java/nl/endran/kotlinexample/MainActivity.kt
+++ Sources/KotlinExample/app/src/main/java/nl/endran/kotlinexample/MainActivity.kt
@@ -18,6 +18,8 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         val fab = findViewById(R.id.fab) as FloatingActionButton
-        fab.setOnClickListener { view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show() }
+
+        val example = Example(fab)
+        example.doMagic()
     }
 }
