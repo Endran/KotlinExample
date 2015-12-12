731c3d9aa39ad38511f5522cd1894704d0500835
David Hardy <david.hardy@philips.com>
2015-12-12 21:35:46 +0100

    Step 6; Show higher order functions and lambda usage

--- Sources/KotlinExample/app/src/main/java/nl/endran/kotlinexample/Example.kt
+++ Sources/KotlinExample/app/src/main/java/nl/endran/kotlinexample/Example.kt
@@ -1,13 +1,10 @@
 package nl.endran.kotlinexample
 
 import android.support.design.widget.FloatingActionButton
-import android.support.design.widget.Snackbar
-import android.view.View
 
 class Example(val fab: FloatingActionButton) {
 
     fun doMagic() {
-        val i = 42
-        fab.showSnackBarOnClick(i)
+        fab.showSnackBarOnClick(42, 37) { x, y -> x + y }
     }
 }
--- Sources/KotlinExample/app/src/main/java/nl/endran/kotlinexample/ViewExtensions.kt
+++ Sources/KotlinExample/app/src/main/java/nl/endran/kotlinexample/ViewExtensions.kt
@@ -3,6 +3,6 @@ package nl.endran.kotlinexample
 import android.support.design.widget.Snackbar
 import android.view.View
 
-fun View.showSnackBarOnClick(message: Int) {
-    setOnClickListener { Snackbar.make(it, "Value is $message", Snackbar.LENGTH_LONG).setAction("Action", null).show() }
+fun View.showSnackBarOnClick(a: Int, b: Int, operation: (x: Int, y: Int) -> Int) {
+    setOnClickListener { Snackbar.make(it, "Value is ${operation(a, b)}", Snackbar.LENGTH_LONG).setAction("Action", null).show() }
 }
