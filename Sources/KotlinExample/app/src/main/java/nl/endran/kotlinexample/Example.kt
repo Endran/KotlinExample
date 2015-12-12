5b84b4ddc42b8696773f96406e3c8729601ef93c
David Hardy <david.hardy@philips.com>
2015-12-12 21:43:46 +0100

    Step 7; Show default extensions

--- Sources/KotlinExample/app/src/main/java/nl/endran/kotlinexample/Example.kt
+++ Sources/KotlinExample/app/src/main/java/nl/endran/kotlinexample/Example.kt
@@ -5,6 +5,10 @@ import android.support.design.widget.FloatingActionButton
 class Example(val fab: FloatingActionButton) {
 
     fun doMagic() {
-        fab.showSnackBarOnClick(42, 37) { x, y -> x + y }
+        val listOf = listOf(1, 2, 3, 4, 5)
+        val reduced = listOf.reduce(function())
+        fab.showSnackBarOnClick(0, reduced, function())
     }
+
+    private fun function() = { x : Int, y : Int -> x + y }
 }
